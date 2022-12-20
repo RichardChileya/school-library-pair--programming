2c54b2a203292f3c9f5021e7da5c55bb5ad816f7
Emre Karayaman <karayaman.emre@outlook.com>
2022-12-20 14:23:19 +0300

    decorate file error fix

--- decorator.rb
+++ decorator.rb
@@ -17,11 +17,10 @@ class TrimmerDecorator < Decorator
   def correct_name
     @nameable.correct_name[0...10]
   end
+end
 
 class CapitalizeDecorator < Decorator
   def correct_name
     @nameable.correct_name.capitalize
   end
-end
-
 end
