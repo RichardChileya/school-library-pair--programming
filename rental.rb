da08a0efb64b7487bece80cfb9e483c92541b710
Emre Karayaman <karayaman.emre@outlook.com>
2022-12-21 21:15:53 +0300

    ui created

--- rental.rb
+++ rental.rb
@@ -1,7 +1,11 @@
-class Rental
-  attr_accessor :date, :book, :person
+require_relative './book'
+require_relative './person'
 
-  def initialize(date, book, person)
+class Rental
+  attr_reader :book, :person
+  attr_accessor :date
+
+  def initialize(person, book, date)
     @date = date
     @book = book
     @person = person
