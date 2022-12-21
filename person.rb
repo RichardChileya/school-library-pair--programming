6ee557b4890a57958556d8570c2ca041adbf5c6b
Emre Karayaman <karayaman.emre@outlook.com>
2022-12-21 13:06:37 +0300

    required changes made

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ require './nameable'
 require './decorator'
 
 class Person < Nameable
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -10,6 +10,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
     super()
   end
 
