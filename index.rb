84f0e22be14614cc7648b7f2af653d1c1683af55
RichardChileya <richardchileyawk@gmail.com>
2023-01-02 14:38:34 +0200

    fix linter error

--- index.rb
+++ index.rb
@@ -5,13 +5,13 @@ def menu
   puts 'Please choose an option by entering a number:'
 
   list = {
-      '1' => 'List all books.',
-      '2' => 'List all people.',
-      '3' => 'Create a person.',
-      '4' => 'Create a book.',
-      '5' => 'Create a rental.',
-      '6' => 'List all rentals for a given person id.',
-      '7' => 'Exit app.'
+    '1' => 'List all books.',
+    '2' => 'List all people.',
+    '3' => 'Create a person.',
+    '4' => 'Create a book.',
+    '5' => 'Create a rental.',
+    '6' => 'List all rentals for a given person id.',
+    '7' => 'Exit app.'
   }
 
   list.each do |index, string|
