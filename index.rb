9751fdd879bcd1e1ec4f2400c467721d7708273a
Emre Karayaman <karayaman.emre@outlook.com>
2023-01-02 11:07:52 +0300

    created index

---
+++ index.rb
@@ -0,0 +1,21 @@
+require './app'
+require './user_choice'
+def menu
+  puts 'Please choose an option by entering a number: '
+
+  list = {
+      '1' => 'List all books',
+      '2' => 'List all people',
+      '3' => 'Create a person',
+      '4' => 'Create a book',
+      '5' => 'Create a rental',
+      '6' => 'List all rentals for a given person id',
+      '7' => 'Exit'
+  }
+
+  list.each do |index, string|
+    puts "#{index} - #{string}"
+  end
+
+  user_choice(gets.chomp.to_i)
+end
