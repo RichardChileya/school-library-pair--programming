e453502b18abc7ea9467fd997d7f64b0b34422b1
Emre Karayaman <karayaman.emre@outlook.com>
2023-01-02 20:35:41 +0300

    file for storing data created

--- data/data.rb
+++ data/data.rb
@@ -10,21 +10,164 @@ def save_book(title, author)
     author: author
   }
 
-  if File.exist?('./books.json')
-    file = File.open('./books.json')
+  if File.exist?('./data/books.json')
+    file = File.open('./data/books.json')
 
-    if file.empty?
+    if file.size.zero?
       book = [obj]
     else
-      book = JSON.parse(File.open('./books.json'))
+      book = JSON.parse(File.read('./data/books.json'))
       book << obj
     end
 
     file.close
 
-    addFile = File.open('./books.json', 'w')
-    addFile.write(JSON.generate(book))
-    addFile.close
-  end  
+    addfile = File.open('./data/books.json', 'w')
+    addfile.write(JSON.generate(book))
+    addfile.close
+  end
 end
-    
+
+def load_books
+  if File.exist?('./data/books.json')
+    file = File.open('./data/books.json')
+
+    if file.size.zero?
+      'No book records yet.'
+    else
+      books = JSON.parse(File.read('./data/books.json'))
+
+      books.each do |book|
+        book = Book.new(book['title'], book['author'])
+        @books << book
+      end
+    end
+
+    file.close
+
+  else
+    puts 'Please add books.'
+  end
+  puts 'Books:'
+  @books.each { |b| puts "Book Title: #{b.title}, Book Author: #{b.author}" } unless @books.empty?
+end
+
+def load_person
+  if File.exist?('./data/person.json')
+    file = File.open('./data/person.json')
+    if File.empty?('./data/person.json')
+      puts 'No person records yet.'
+    else
+      people = JSON.parse(File.read('./data/person.json'))
+      people.each do |person|
+        if person['option'] == 'Student'
+          student = Student.new(person['age'], person['name'])
+          @people << student
+        else
+          teacher = Teacher.new(person['specialization'], person['age'], person['name'])
+          @people << teacher
+        end
+      end
+    end
+    file.close
+  else
+    puts 'Please add a person.'
+  end
+end
+
+def save_student(name, age, parent_permission)
+  obj = {
+    type: 'Student',
+    name: name,
+    age: age,
+    parent_permission: parent_permission
+  }
+
+  return unless File.exist?('./data/person.json')
+
+  file = File.open('./data/person.json')
+
+  if file.size.zero?
+    student = [obj]
+  else
+    student = JSON.parse(File.read('./data/person.json'))
+    student << obj
+  end
+
+  file.close
+
+  addfile = File.open('./data/person.json', 'w')
+  addfile.write(JSON.generate(student))
+  addfile.close
+end
+
+def save_teacher(name, age, specialization)
+  obj = {
+    type: 'Teacher',
+    name: name,
+    specialization: specialization,
+    age: age,
+    parent_permission: true
+  }
+
+  return unless File.exist?('./data/person.json')
+
+  file = File.open('./data/person.json')
+
+  if file.size.zero?
+    teacher = [obj]
+  else
+    teacher = JSON.parse(File.read('./data/person.json'))
+    teacher << obj
+  end
+
+  file.close
+
+  addfile = File.open('./data/person.json', 'w')
+  addfile.write(JSON.generate(teacher))
+  addfile.close
+end
+
+def load_rentals
+  if File.exist?('./data/rentals.json')
+    file = File.open('./data/rentals.json')
+
+    if file.size.zero?
+      puts 'No rentals yet.'
+    else
+      rentals = JSON.parse(File.read('./data/rentals.json'))
+      puts 'Rented books: '
+      rentals.each do |rental|
+        puts "Name: #{rental['person']}, Book: #{rental['book']} on: #{rental['date']}"
+      end
+    end
+    file.close
+  else
+    puts 'No rent records yet.'
+  end
+end
+
+def save_rental(date, people, book)
+  obj = {
+    date: date,
+    people: people.name,
+    book: book.title
+  }
+
+  return unless File.exist?('./data/rentals.json')
+
+  file = File.open('./data/rentals.json')
+
+  if file.size.zero?
+    rental = [obj]
+  else
+    rental = JSON.parse(File.read('./data/rentals.json'))
+    rental << obj
+  end
+
+  file.close
+
+  addfile = File.open('./data/rentals.json', 'w')
+  addfile.write(JSON.generate(rental))
+  addfile.close
+end
