eb7b2eec70eb87c57654d31ee5ff550baf7ac7ee
Emre Karayaman <karayaman.emre@outlook.com>
2023-01-03 16:55:51 +0300

    Update teacher_spec.rb

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -8,10 +8,6 @@ describe Teacher do
       expect(teacher.age).to eq 33
     end
 
-    it 'check the specialization' do
-      expect(teacher.specialization).to eq 'Ruby'
-    end
-
     it 'check the name' do
       expect(teacher.name).to eq 'Emre'
     end
