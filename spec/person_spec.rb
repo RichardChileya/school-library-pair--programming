74090df415fb3d0daf40acae7bce02a67db0fcb1
RichardChileya <richardchileyawk@gmail.com>
2023-01-03 15:47:36 +0200

    Add person test file

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,13 +1,13 @@
 require './person'
-describe Person do 
-  context 'create a person' do 
+describe Person do
+  context 'create a person' do
     person = Person.new(27, 'Ricardo')
 
-    it  ' show age' do 
+    it ' show age' do
       expect(person.age).to eq 27
     end
 
-    it 'show name' do 
+    it 'show name' do
       expect(person.name).to eq 'Ricardo'
     end
   end
